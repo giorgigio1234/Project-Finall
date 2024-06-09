e58cfaae97b27844bc846b43a3a75149145be674
George <daraselia2009@gmail.com>
2024-06-09 09:42:33 +0400

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const questions = [
     {
         question: "What is the tallest mountain in the world?",
-        answers: [
+        answer: [
             {text: "A) Mount Kilimanjaro", correct: false},
             {text: "B) Mount Everest", correct: true},
             {text: "C) Mount McKinley", correct: false},
@@ -10,7 +10,7 @@ const questions = [
     },
     {
         question: "Who is credited with the invention of the telephone?",
-        answers: [
+        answer: [
             {text: "A) Alexander Graham Bell", correct: true},
             {text: "B) Thomas Edison", correct: false},
             {text: "C) Nikola Tesla", correct: false},
@@ -19,7 +19,7 @@ const questions = [
     },
     {
         question: "What is the capital city of Australia?",
-        answers: [
+        answer: [
             {text: "A) Sydney", correct: false},
             {text: "B) Melbourne", correct: false},
             {text: "C) Canberra", correct: true},
@@ -28,7 +28,7 @@ const questions = [
     },
     {
         question: "Which planet is known as the Red Planet?",
-        answers: [
+        answer: [
             {text: "A) Jupiter", correct: false},
             {text: "B) Mars", correct: true},
             {text: "C) Venus", correct: false},
@@ -37,7 +37,7 @@ const questions = [
     },
     {
         question: "Who painted the Mona Lisa?",
-        answers: [
+        answer: [
             {text: "A) Vincent van Gogh", correct: false},
             {text: "B) Pablo Picasso", correct: false},
             {text: "C) Leonardo da Vinci", correct: true},
@@ -46,7 +46,7 @@ const questions = [
     },
     {
         question: "Who was the first man to walk on the moon?",
-        answers: [
+        answer: [
             {text: "A) Neil Armstrong", correct: true},
             {text: "B) Buzz Aldrin", correct: false},
             {text: "C) Yuri Gagarin", correct: false},
@@ -55,7 +55,7 @@ const questions = [
     },
     {
         question: "In which year did the Titanic sink?",
-        answers: [
+        answer: [
             {text: "A) 1909", correct: false},
             {text: "B) 1912", correct: true},
             {text: "C) 1921", correct: false},
@@ -66,7 +66,7 @@ const questions = [
 
 const questionElement = document.getElementById("question")
 const answerButton = document.getElementById("answer-box")
-const nextbutton = document.getElementById("next-btn")
+const nextbutton = document.getElementById("Next-btn")
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -79,10 +79,83 @@ function startQuiz(){
 }
 
 function showQuestion(){
-    let currentQuestionIndex = questions[currentQuestionIndex];
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1
-    questionElement.innerHTML = questionNo + ". " + currentQuestionIndex.
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
     question;
 
-    currentQuestionIndex.answers.forEach(answers => {})
+
+    currentQuestion.answer.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+    
+    
+
 }
+
+
+function resetState(){
+    nextbutton.style.display = "none";
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct")
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct")
+        }
+        button.disabled = true;
+    });
+    nextbutton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
+    nextbutton.innerHTML = "Play Again";
+    nextbutton.style.display = "block";
+
+}
+
+
+function handleNextButton(){
+    currentQuestionIndex++
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+
+
+nextbutton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
+
